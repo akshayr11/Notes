21a8b092e31dc247e23c8d279c734b782156a8a9
Akshay Rajput <akshay.rajput@coditas.com>
2018-11-19 17:26:53 +0530

    crud via terminal for a node app using yargs

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
-const fs = require("fs");
-const os = require("os");
-const _ = require("lodash");
 const notes = require("./notes");
 const yargs = require("yargs");
-// var user = os.userInfo();
-// fs.appendFile("greetings.txt", "Hello World!!", err => {
-// 	if (err) {
-// 		console.log("Unable to write to the file");
-// 	}
-// 	console.log(`Hello ${user.username.toUpperCase()}`);
-// });
-// var res = notes.addNote(3, 5);
-// console.log(res);
-
-// var users = ["Akshay", "Akshay", "Akshay", "Doe"];
-// var filterdUsers = _.uniq(users);
-// console.log(filterdUsers);
 
 const argv = yargs.argv;
 const command = process.argv[2];
 if (command === "add") {
-	notes.addNote(argv.title, argv.body);
+	const addStatus = notes.addNote(argv.title, argv.body);
+	console.log(addStatus);
 } else if (command === "list") {
-	notes.getAll();
+	const NotesList = notes.getAll();
+	console.log(NotesList);
 } else if (command === "read") {
-	notes.readNote(argv.title);
-} else if (command === "delete") {
-	notes.removeNote(argv.title);
+	const requestedNote = notes.readNote(argv.title);
+	console.log(requestedNote);
+} else if (command === "remove") {
+	const status = notes.removeNote(argv.title);
+	console.log(status);
 } else {
 	console.log("Command Not Recognised");
 }
--- notes.js
+++ notes.js
@@ -1,22 +1,69 @@
+const fs = require("fs");
+
+fetchNotes = () => {
+	try {
+		const notesString = fs.readFileSync("notes-Data.json");
+		return JSON.parse(notesString);
+	} catch (error) {
+		return [];
+	}
+};
+
+saveNotes = notes => {
+	if (notes) {
+		fs.writeFileSync("notes-Data.json", JSON.stringify(notes));
+	}
+};
+
 const addNote = (title, body) => {
-	console.log("Adding Note", title, body);
+	let notes = fetchNotes();
+	const note = {
+		title,
+		body
+	};
+	var duplicateNotes = notes.findIndex(note => note.title === title);
+	if (duplicateNotes !== -1) {
+		return `Note with the title ${title} already exist.`;
+	} else {
+		notes.push(note);
+		saveNotes(notes);
+		return `Note with the title: ${title} created successfully.`;
+	}
 };
 
 const getAll = () => {
-	console.log("Fetching all notes");
+	let notes = fetchNotes();
+	if (!notes) {
+		return `No Notes found`;
+	} else {
+		return notes;
+	}
 };
 
-const readNote = () => {
-	console.log("Getting note");
+const readNote = (title) => {
+    const notes = fetchNotes();
+    var noteIndex = notes.findIndex(note => note.title === title);
+    if (noteIndex === -1) {
+		return `Note with the title ${title} doesn't exist.`;
+	} else {
+		return `Requested Note ${title}: ${notes[noteIndex].body}`;
+    }
 };
-const removeNote = () => {
-	console.log("Note Deleted");
+const removeNote = title => {
+	var notes = fetchNotes();
+	var noteindex = notes.findIndex(note => note.title === title);
+	if (noteindex === -1) {
+		return `Note with the title ${title} doesn't exist.`;
+	} else {
+		notes.splice(noteindex, 1);
+		saveNotes(notes);
+		return `Note with the title ${title} removed sucessfully.`;
+	}
 };
-
 
 module.exports = {
 	addNote,
-    getAll,
-    readNote,
-    removeNote
+	getAll,
+	readNote,
+	removeNote
 };
